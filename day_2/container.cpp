9e4b833cabdd59687ab0421e0ca6efcbd92a1612
SHRobin123 <robinsabbir123@gmail.com>
2025-07-14 20:35:37 -0700

    Competitive programming_02

--- day_2/container.cpp
+++ day_2/container.cpp
@@ -2,12 +2,14 @@
 #include <vector>
 using namespace std;
 
-int maxArea(vector<int>& height) {
+int maxArea(vector<int> &height)
+{
     int left = 0;                  // pointer from the left side
     int right = height.size() - 1; // pointer from the right side
     int maxWater = 0;              // to keep track of the maximum water
 
-    while (left < right) {
+    while (left < right)
+    {
         // height is limited by the shorter line
         int h = min(height[left], height[right]);
         // width is the distance between the lines
@@ -16,14 +18,18 @@ int maxArea(vector<int>& height) {
         int currentArea = h * w;
 
         // update maxWater if current area is bigger
-        if (currentArea > maxWater) {
+        if (currentArea > maxWater)
+        {
             maxWater = currentArea;
         }
 
         // move the pointer which has the shorter line
-        if (height[left] < height[right]) {
+        if (height[left] < height[right])
+        {
             left++;
-        } else {
+        }
+        else
+        {
             right--;
         }
     }
@@ -31,8 +37,9 @@ int maxArea(vector<int>& height) {
     return maxWater;
 }
 
-int main() {
-    vector<int> height = {1,8,6,2,5,4,8,3,7};
+int main()
+{
+    vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
     cout << "Max water the container can store: " << maxArea(height) << endl;
     return 0;
 }
