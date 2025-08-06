9fc509ba19c6ed4d513aff82eed453d5631b0859
SHRobin123 <robinsabbir123@gmail.com>
2025-08-06 20:35:30 +0600

    CP_01

---
+++ Week__10/competitive_program-01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while ((t--) > 0)
+    { // t times
+        int n;
+        cin >> n;
+
+        cout << (n - 1) << "\n";
+    }
+
+    return 0;
+}
+/*
+INPUT: 3
+2 4 5
+
+OUTPUT:
+1
+3
+4
+*/
